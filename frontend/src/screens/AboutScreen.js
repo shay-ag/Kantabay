7aed3841c3f1f4314de5951463a78b56f861a465
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-19 15:22:16 +0530

    changes in about screen

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button, Image } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import './AboutScreen.css';
 
