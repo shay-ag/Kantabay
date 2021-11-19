283687c3d47b478c1e6f8798f2c09eb489b7e764
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-19 17:58:31 +0530

    added more routes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container, Stack } from 'react-bootstrap';
 
 import './Header.css';
@@ -8,7 +9,8 @@ const Header = () => {
         <header>
             <Navbar expand="lg" collapseOnSelect>
                 <Container>
-                <Navbar.Brand href="#home">
+                <LinkContainer to="/">
+                <Navbar.Brand>
                 <img
                     src="images/logo.png"
                     width="90"
@@ -17,19 +19,36 @@ const Header = () => {
                     alt="React Bootstrap logo"
                 />
                 </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                 <Nav className="ms-auto my-2 my-lg-0">
                 
                 <Stack direction="horizontal" gap={4}>
-                <Nav.Link href="#home"><div className="custom-nav-item">Home</div></Nav.Link>
-                <Nav.Link href="#features"><div className="custom-nav-item">About</div></Nav.Link>
-                <Nav.Link href="#pricing"><div className="custom-nav-item">Find Maids</div></Nav.Link>
-                <Nav.Link href="#home"><div className="custom-nav-item">Reviews</div></Nav.Link>
-                <Nav.Link href="#features"><div className="custom-nav-item">Blogs</div></Nav.Link>
-                <Nav.Link href="#pricing"><div className="custom-nav-item">Contact</div></Nav.Link>
-                <Nav.Link href="#pricing"><div className="custom-nav-item">Join Us</div></Nav.Link>
-                <Nav.Link href="#pricing"><div className="custom-nav-item">SIGN IN</div></Nav.Link>
+                <LinkContainer to="/">
+                <Nav.Link ><div className="custom-nav-item">Home</div></Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/about">
+                <Nav.Link ><div className="custom-nav-item">About</div></Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/maids">
+                <Nav.Link ><div className="custom-nav-item">Find Maids</div></Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/reviews">
+                <Nav.Link ><div className="custom-nav-item">Reviews</div></Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/blogs">
+                <Nav.Link ><div className="custom-nav-item">Blogs</div></Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/contact">
+                <Nav.Link ><div className="custom-nav-item">Contact</div></Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/joinus">
+                <Nav.Link ><div className="custom-nav-item">Join Us</div></Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/signup">
+                <Nav.Link ><div className="custom-nav-item">Sign In</div></Nav.Link>
+                </LinkContainer>
                 </Stack>
                 </Nav>
                 </Navbar.Collapse>
