72e4421cdea5bf2df7fb8bb5ba78c9c60819792c
Shashwat Anand <72724909+ShashwatAnand15@users.noreply.github.com>
2021-11-22 00:09:16 +0530

    Update auth.js

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -1,1 +1,23 @@
-// This is for authentication code
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const compression = require('compression');
+const uuid = require('uuid');
+const config = require('../config/appconfig');
+const Logger = require('../utils/logger.js');
+
+const logger = new Logger();
+const app = express();
+app.set('config', config); // the system configrationsx
+app.use(bodyParser.json());
+app.use(require('method-override')());
+
+app.use(compression());
+app.use(cors());
+const swagger = require('../utils/swagger');
+
+
+process.on('SIGINT', () => {
+	logger.log('stopping the server', 'info');
+	process.exit();
+});
