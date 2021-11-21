a7e9eb25a7a23aed465ff7b326bf2a3c74e40235
Shashwat Anand <72724909+ShashwatAnand15@users.noreply.github.com>
2021-11-22 00:10:49 +0530

    Update auth.js

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -21,3 +21,24 @@ process.on('SIGINT', () => {
 	logger.log('stopping the server', 'info');
 	process.exit();
 });
+app.set('db', require('../models/index.js'));
+
+app.set('port', process.env.DEV_APP_PORT);
+app.use('/api/docs', swagger.router);
+
+app.use((req, res, next) => {
+	req.identifier = uuid();
+	const logString = `a request has been made with the following uuid [${req.identifier}] ${req.url} ${req.headers['user-agent']} ${JSON.stringify(req.body)}`;
+	logger.log(logString, 'info');
+	next();
+});
+
+app.use(require('../router'));
+
+app.use((req, res, next) => {
+	logger.log('the url you are trying to reach is not hosted on our server', 'error');
+	const err = new Error('Not Found');
+	err.status = 404;
+	res.status(err.status).json({ type: 'error', message: 'the url you are trying to reach is not hosted on our server' });
+	next(err);
+});
