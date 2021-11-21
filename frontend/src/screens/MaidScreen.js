ea550adb06e629a4958bbe1b9ef5d05eca2ca308
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-21 17:49:21 +0530

    completed MaidScreen

--- frontend/src/screens/MaidScreen.js
+++ frontend/src/screens/MaidScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
+import Maid from '../components/Maid';
 import './MaidScreen.css';
+import maids from '../maids';
 
 const MaidScreen = () => {
     return (
@@ -10,7 +12,13 @@ const MaidScreen = () => {
                 <h1 className="title">Maids in <span>Kanta</span>Bay<span>...</span></h1>
             </Row>
             <Row className="content-box">
-                
+                {maids.map( (maid) => {
+                    return (
+                        <Col sm={12} md={6} lg={4} xl={3}>
+                            <Maid maid={maid} />
+                        </Col>
+                    )
+                })}
             </Row>
         </Container>
     )
