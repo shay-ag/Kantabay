bcb9d9a5941b5f6baf77ac118988dc5a32724296
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-21 21:59:36 +0530

    working on BlogScreen

---
+++ frontend/src/components/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { } from 'react-bootstrap';
+
+import './Blog.css';
+
+const Blog = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Blog
--- frontend/src/screens/BlogScreen.js
+++ frontend/src/screens/BlogScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
+import Blog from '../components/Blog';
 import './BlogScreen.css';
+import blogs from '../blogs';
 
 const BlogScreen = () => {
     return (
@@ -10,10 +12,14 @@ const BlogScreen = () => {
                 <h1 className="title">Blogs on <span>Kanta</span>Bay<span>...</span></h1>
             </Row>
             <Row className="content-box">
-                
+                {blogs.map( (blog) => {
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Blog blog={blog} />
+                    </Col>
+                })}
             </Row>
         </Container>
     )
 }
 
-export default BlogScreen
+export default BlogScreen;
