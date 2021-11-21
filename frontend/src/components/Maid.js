fbdfbe87ca79f0de187283a76fecbd7de82f117b
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-21 18:49:53 +0530

    implementing maid cards

--- frontend/src/components/Maid.js
+++ frontend/src/components/Maid.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 import './Maid.css';
 
-const Maid = () => {
+const Maid = ( { maid } ) => {
     return (
-        <div>
-            
-        </div>
+        <Card className='my-3 mx-2 mb-5 p-1'>
+            <a href={`/maid/${maid._id}`}>
+                <Card.Img src={maid.image} variant='bottom' width="100" height="200"/>
+            </a>
+
+            <Card.Body>
+                <a href={`/maid/${maid._id}`}>
+                    <Card.Title as='div'>
+                        <strong>{maid.name}</strong>
+                    </Card.Title>
+                </a>
+
+                <Card.Text as='div'>
+                    <div className='my-3'>
+                        {maid.description}
+                    </div>
+                    <div>
+                        Experience: {maid.experience}
+                    </div>
+                </Card.Text>
+
+                <Card.Text as='div'>
+                    Rating: {maid.rating}
+                </Card.Text>
+
+            </Card.Body>
+        </Card>
     )
 }
 
