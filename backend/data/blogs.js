5178b90a2630eef42704617f0bb95c99c51c09cd
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-22 21:08:55 +0530

    added the required packages for backend

---
+++ backend/data/blogs.js
@@ -0,0 +1,68 @@
+const blogs = [
+    {
+        _id: '1',
+        image: '/images/blogs/blog1.jpg',
+        title: 'Quality Cleaning Services',
+        content: 'With various maids services on the market comes various amounts of confusion. You never know who to trust and who will give you a quality clean. With MaidsApp, you don’t have that issue. MaidsApp will provide you with the best clean that you could have ever thought of. Whether you have an important occasion coming up in your home, or you have an important meeting in your office, you can call MaidsApp to give you a quality clean that you would not have gotten anywhere else. Our quality cleaning services are unbeatable. With the top quality cleaning services that our maids offer, you will be calling us back for sure! MaidsApp allows clients to schedule a maid to come and clean their home or office on demand, or in the future. We are very flexible with our cleaning times and availability, because we are always doing our best to make sure that our clients are happy and satisfied. If you decide you want your home or office cleaned on the same day, we can make that happen. On the other hand, if you want your home or office cleaned on a day in advance, we can make that happen as well. With just a few simple taps on your smartphone, you can have one of our professional maids at your doorstep whenever you want! If you need a maid to clean with high quality cleaning services, they will be there on your schedule. The short notices do not impact quality cleaning services offered by MaidsApp',
+        author: 'KantaBay Team',
+        date: '20th Nov 2021',
+    },
+    {
+        _id: '2',
+        image: '/images/blogs/blog2.jpg',
+        title: 'KantaBay before COVID-19',
+        content: 'It seems like we are living with COVID-19 forever because it has prevailed so much and is here for so long. And also, it has no mood to go back from where it came. It is obvious now that Corona is not going anywhere. Instead, we have to accept it and need to live with it because there is no other option left. Considering the world where Corona has wreaked havoc, people think twice before hiring a maid. The health of the maid they ought to hire is a big concern for them. Before COVID-19, MaidsApp was slightly different because we changed the policies. Unlike now, there was no need for preventive measures before Corona. The maids used to go without masks and sanitizers to all the places. There is only one thing that never changed, and that is the professional attitude of the MaidsApp. The company was always a proficient cleaning service and always thought about the ease and convenience of the company. Now the company is more vigilant about its customer health and wellbeing. For that purpose, all the maids got their vaccines and are completely secure and free from Corona. Therefore, there is no need to worry about the sanitation and hygiene of the cleaners.', 
+        author: 'Shay Ag',
+        date: '19th Nov 2021',
+    },
+    {
+        _id: '3',
+        image: '/images/blogs/blog3.jpg',
+        title: 'Maids on Demand',
+        content: 'It seems like we are living with COVID-19 forever because it has prevailed so much and is here for so long. And also, it has no mood to go back from where it came. It is obvious now that Corona is not going anywhere. Instead, we have to accept it and need to live with it because there is no other option left. Considering the world where Corona has wreaked havoc, people think twice before hiring a maid. The health of the maid they ought to hire is a big concern for them. Before COVID-19, MaidsApp was slightly different because we changed the policies. Unlike now, there was no need for preventive measures before Corona. The maids used to go without masks and sanitizers to all the places. There is only one thing that never changed, and that is the professional attitude of the MaidsApp. The company was always a proficient cleaning service and always thought about the ease and convenience of the company. Now the company is more vigilant about its customer health and wellbeing. For that purpose, all the maids got their vaccines and are completely secure and free from Corona. Therefore, there is no need to worry about the sanitation and hygiene of the cleaners.', 
+        author: 'Angela Yu',
+        date: '21th Nov 2021',
+    },
+    {
+        _id: '4',
+        image: '/images/blogs/blog4.jpg',
+        title: 'Maid Services in KantaBay',
+        content: 'It seems like we are living with COVID-19 forever because it has prevailed so much and is here for so long. And also, it has no mood to go back from where it came. It is obvious now that Corona is not going anywhere. Instead, we have to accept it and need to live with it because there is no other option left. Considering the world where Corona has wreaked havoc, people think twice before hiring a maid. The health of the maid they ought to hire is a big concern for them. Before COVID-19, MaidsApp was slightly different because we changed the policies. Unlike now, there was no need for preventive measures before Corona. The maids used to go without masks and sanitizers to all the places. There is only one thing that never changed, and that is the professional attitude of the MaidsApp. The company was always a proficient cleaning service and always thought about the ease and convenience of the company. Now the company is more vigilant about its customer health and wellbeing. For that purpose, all the maids got their vaccines and are completely secure and free from Corona. Therefore, there is no need to worry about the sanitation and hygiene of the cleaners.', 
+        author: 'Rocky Farms',
+        date: '20th Nov 2021',
+    },
+    {
+        _id: '5',
+        image: '/images/blogs/blog5.jpg',
+        title: 'Maid Service and House Cleaning',
+        content: 'It seems like we are living with COVID-19 forever because it has prevailed so much and is here for so long. And also, it has no mood to go back from where it came. It is obvious now that Corona is not going anywhere. Instead, we have to accept it and need to live with it because there is no other option left. Considering the world where Corona has wreaked havoc, people think twice before hiring a maid. The health of the maid they ought to hire is a big concern for them. Before COVID-19, MaidsApp was slightly different because we changed the policies. Unlike now, there was no need for preventive measures before Corona. The maids used to go without masks and sanitizers to all the places. There is only one thing that never changed, and that is the professional attitude of the MaidsApp. The company was always a proficient cleaning service and always thought about the ease and convenience of the company. Now the company is more vigilant about its customer health and wellbeing. For that purpose, all the maids got their vaccines and are completely secure and free from Corona. Therefore, there is no need to worry about the sanitation and hygiene of the cleaners.', 
+        author: 'KantaBay Team',
+        date: '19th Nov 2021',
+    },
+    {
+        _id: '6',
+        image: '/images/blogs/blog6.jpg',
+        title: 'Maid Service Cleaning',
+        content: 'It seems like we are living with COVID-19 forever because it has prevailed so much and is here for so long. And also, it has no mood to go back from where it came. It is obvious now that Corona is not going anywhere. Instead, we have to accept it and need to live with it because there is no other option left. Considering the world where Corona has wreaked havoc, people think twice before hiring a maid. The health of the maid they ought to hire is a big concern for them. Before COVID-19, MaidsApp was slightly different because we changed the policies. Unlike now, there was no need for preventive measures before Corona. The maids used to go without masks and sanitizers to all the places. There is only one thing that never changed, and that is the professional attitude of the MaidsApp. The company was always a proficient cleaning service and always thought about the ease and convenience of the company. Now the company is more vigilant about its customer health and wellbeing. For that purpose, all the maids got their vaccines and are completely secure and free from Corona. Therefore, there is no need to worry about the sanitation and hygiene of the cleaners.', 
+        author: 'Gupta Services',
+        date: '20th Nov 2021',
+    },
+    {
+        _id: '7',
+        image: '/images/blogs/blog7.jpg',
+        title: 'Cleanliness is next to Godliness',
+        content: 'It seems like we are living with COVID-19 forever because it has prevailed so much and is here for so long. And also, it has no mood to go back from where it came. It is obvious now that Corona is not going anywhere. Instead, we have to accept it and need to live with it because there is no other option left. Considering the world where Corona has wreaked havoc, people think twice before hiring a maid. The health of the maid they ought to hire is a big concern for them. Before COVID-19, MaidsApp was slightly different because we changed the policies. Unlike now, there was no need for preventive measures before Corona. The maids used to go without masks and sanitizers to all the places. There is only one thing that never changed, and that is the professional attitude of the MaidsApp. The company was always a proficient cleaning service and always thought about the ease and convenience of the company. Now the company is more vigilant about its customer health and wellbeing. For that purpose, all the maids got their vaccines and are completely secure and free from Corona. Therefore, there is no need to worry about the sanitation and hygiene of the cleaners.', 
+        author: 'Anand Services',
+        date: '19th Nov 2021',
+    },
+    {
+        _id: '8',
+        image: '/images/blogs/blog8.jpg',
+        title: 'Keeping Your house Clean is must',
+        content: 'It seems like we are living with COVID-19 forever because it has prevailed so much and is here for so long. And also, it has no mood to go back from where it came. It is obvious now that Corona is not going anywhere. Instead, we have to accept it and need to live with it because there is no other option left. Considering the world where Corona has wreaked havoc, people think twice before hiring a maid. The health of the maid they ought to hire is a big concern for them. Before COVID-19, MaidsApp was slightly different because we changed the policies. Unlike now, there was no need for preventive measures before Corona. The maids used to go without masks and sanitizers to all the places. There is only one thing that never changed, and that is the professional attitude of the MaidsApp. The company was always a proficient cleaning service and always thought about the ease and convenience of the company. Now the company is more vigilant about its customer health and wellbeing. For that purpose, all the maids got their vaccines and are completely secure and free from Corona. Therefore, there is no need to worry about the sanitation and hygiene of the cleaners.', 
+        author: 'KantaBay Team',
+        date: '19th Nov 2021',
+    }
+];
+
+export default blogs;
