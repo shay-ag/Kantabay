e559881439eadc499b918f2207f62098973c479a
Shashwat Gupta <62785479+shashwat0105@users.noreply.github.com>
2021-11-24 01:35:51 +0530

    Added register a new user in userController.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -23,3 +23,37 @@ const authUser = asyncHandler( async (req, res) => {
         throw new Error('Invalid email or password');
     }
 });
+
+// register a user
+const registerUser = asyncHandler( async (req, res) => {
+    
+    const { name, email, password }= req.body
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        res.status(400);
+        throw new Error('User Already Exists');
+    }
+
+    const user = await User.create({
+        name, 
+        email,
+        password
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else{
+        res.status(400);
+        throw new Error('Invalid User Data');
+    }
+
+});
+
