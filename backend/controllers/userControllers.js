caf3e9148f9e1dfa26ec5083bfd06a5cff40760d
Shashwat Gupta <62785479+shashwat0105@users.noreply.github.com>
2021-11-24 01:30:25 +0530

    Update userController.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,1 +1,25 @@
-k
+import asyncHandler from  'express-async-handler';
+
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateWebToken.js';
+
+// logging in a user
+const authUser = asyncHandler( async (req, res) => {
+    
+    const { email, password }= req.body
+
+    const user = await User.findOne({ email });
+
+    if(user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else{
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+});
