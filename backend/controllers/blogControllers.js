e5ece8cda2ea07a6f6d17220aa98ef1b31d416e5
Shashwat Gupta <62785479+shashwat0105@users.noreply.github.com>
2021-11-24 01:29:01 +0530

    Update blogController.js

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -1,1 +1,12 @@
-k
+import asyncHandler from  'express-async-handler';
+
+import Blog from '../models/blogModel.js';
+
+const getBlogs = asyncHandler( async (req, res) => {
+    
+    const blogs = await Blog.find({})
+    
+    res.json(blogs);
+});
+
+
