79925c8292a8bd34b667bd091aae208238dbafe0
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-24 22:30:16 +0530

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import path from 'path';
 
 import connectDB from './config/db.js';
 
@@ -16,13 +17,21 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-
 app.use('/api/maids', maidRoutes);
 app.use('/api/blogs', blogRoutes);
 app.use('/api/users', userRoutes);
+
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
