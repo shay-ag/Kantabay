40b08fde6a88128cf26c648d2f4c34ea3c59f2f7
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-25 01:31:13 +0530

    renamed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 
-import User from '../models/userModel.js';
+import User from '../models/UserModel.js';
 
 const protect = asyncHandler( async(req, res, next) => {
 
