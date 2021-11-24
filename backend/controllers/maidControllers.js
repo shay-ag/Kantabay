889fe20512af1572b388b49dffbbff066bb4d52b
Shashwat Anand <72724909+ShashwatAnand15@users.noreply.github.com>
2021-11-24 15:44:38 +0530

    Update maidControllers.js

--- backend/controllers/maidControllers.js
+++ backend/controllers/maidControllers.js
@@ -18,14 +18,10 @@ const getMaidDetails = asyncHandler ( async (req, res) => {
     } else {
         res.status(404);
         throw new Error('Maid Not Found');
-        // json({ message: 'Maid Not Found' });
+        
     }
 
-    // const maid = maids.find( (maid) => {
-    //     return maid._id === req.params.maidid;
-    // });
-    //
-    // res.json(maid);
+
 });
 
 export { getMaids, getMaidDetails };
